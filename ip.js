99237d6e21843ca52ef4e8dcdeaf604ce3a3d315
aimetu <41498149+aimetu@users.noreply.github.com>
2022-07-20 21:05:32 +0800

    Update ip.js

--- ip.js
+++ ip.js
@@ -7,15 +7,15 @@ $httpClient.get(url, function (error, response, data) {
   let city = jsonData.city
   let isp = jsonData.isp
   let ip = jsonData.query
-  let lat = jsonData.lat
   let lon = jsonData.lon
+  let lat = jsonData.lat
   let dns = jsonData.dns
-  let dns = jsonData.geo
-  let dns = jsonData.ip
+  let geo = jsonData.geo
+  let ip = jsonData.ip
   
   body = {
     title: "节点信息",
-    content: `运营商：${isp}\nIP：${ip}\n纬度：${lat}\n经度：${lon}\n国家：${emoji}${country} - ${city}\nDNS：${dns} - ${geo}${ip}`,
+    content: `运营商：${isp}\nIP：${ip}\n经纬度：${lon}${lat}\n国家：${emoji}${country} - ${city}\nDNS：${dns} - ${geo}${ip}`,
     icon: "globe.asia.australia.fill",
     backgroundColor: '#00a6ac',
   }
