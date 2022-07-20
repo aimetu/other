4e29a96e64a3cc38b4fa7e8c5f208657b8117ada
aimetu <41498149+aimetu@users.noreply.github.com>
2022-07-20 22:06:19 +0800

    Update network.js

--- network.js
+++ network.js
@@ -1,12 +1,9 @@
 $httpClient.get("https://forge.speedtest.cn/api/location/info", function (error, response, data) {
     let dataObject = JSON.parse(data);
-    let { country, province, isp, city, ip, lon, lat } = dataObject;
+    let { country, province, isp, city, ip } = dataObject;
     isp = `运营商：${isp}`;
     ip = `IP：${ip}`;
     let region = `地区：${country} ${province} ${city}`;
-    lon = `纬度：${lon}`;
-    lat = `经度：${lat}`;
-
     body={
         title: "网络信息",
         content: `${isp}\n${ip}\n${region}`,
