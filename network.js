4f3b1ef3472991421ccf673c289d8d06bf83a1e7
aimetu <41498149+aimetu@users.noreply.github.com>
2022-07-20 22:30:33 +0800

    Update network.js

--- network.js
+++ network.js
@@ -1,9 +1,10 @@
-$httpClient.get("https://forge.speedtest.cn/api/location/info", function (error, response, data) {
+$httpClient.get("https://ip.useragentinfo.com/api", function (error, response, data) {
     let dataObject = JSON.parse(data);
-    let { country, province, isp, city, ip } = dataObject;
+    let { country, short_name, province, city, area, isp, net, ip, code } = dataObject;
     isp = `运营商：${isp}`;
+    net = `服务商：${net}`;
     ip = `IP：${ip}`;
-    let region = `地区：${country} ${province} ${city}`;
+    let region = `地区：${country} ${short_name}${province} ${city} ${area} ${code}`;
     body={
         title: "网络信息",
         content: `${isp}\n${ip}\n${region}`,
