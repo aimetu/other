05008b9c3e210f07461984d84e906ed794943bad
aimetu <41498149+aimetu@users.noreply.github.com>
2022-07-20 22:41:46 +0800

    Update network.js

--- network.js
+++ network.js
@@ -1,13 +1,13 @@
 $httpClient.get("https://ip.useragentinfo.com/json?", function (error, response, data) {
     let dataObject = JSON.parse(data);
-    let { country, short_name, province, city, area, isp, net, ip, code } = dataObject;
+    let { country, short_name, province, city, area, isp, net, ip } = dataObject;
     isp = `运营商：${isp}`;
-    net = `服务商：${net}`;
     ip = `IP：${ip}`;
-    let region = `地区：${country} ${short_name}${province} ${city} ${area} ${code}`;
+    short_name = `简称：${short_name}`;
+    let region = `地区：${country}${province} ${city} ${area}`;
     body={
         title: "网络信息",
-        content: `${isp}\n${net}\n${ip}\n${region}`,
+        content: `${isp}\n${ip}\n${short_name}\n${region}`,
         backgroundColor: "#006c54",
         icon: "antenna.radiowaves.left.and.right.circle.fill",
     }
