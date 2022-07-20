48ca7b6697ab8f88fc680b907dbfa13ee47aa089
aimetu <41498149+aimetu@users.noreply.github.com>
2022-07-21 01:27:34 +0800

    Update ip.js

--- ip.js
+++ ip.js
@@ -4,15 +4,15 @@ $httpClient.get(url, function (error, response, data) {
   let jsonData = JSON.parse(data)
   let country = jsonData.country
   let emoji = getFlagEmoji(jsonData.countryCode)
-	let time = jsonData.timezone
+  let time = jsonData.timezone
   let city = jsonData.city
   let isp = jsonData.isp
   let ip = jsonData.query
-	let lon = jsonData.lon
+  let lon = jsonData.lon
   let lat = jsonData.lat
 
   body = {
-    title: "节点信息",
+    title: "网络信息",
     content: `国家：${emoji} - ${country} - ${city}\n时区：${time}\n运营商：${isp}\nIP：${ip}\n经纬度：${lon} - ${lat}`,
     icon: "globe.asia.australia.fill",
     backgroundColor: '#375830',
