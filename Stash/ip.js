f9deebfac12883243d49d822204141e56afdb2ea
aimetu <41498149+aimetu@users.noreply.github.com>
2024-03-26 11:53:28 +0800

    Update ip.js

--- Stash/ip.js
+++ Stash/ip.js
@@ -7,16 +7,16 @@ $httpClient.get("https://forge.speedtest.cn/api/location/info", function (error,
     city = `市：${city}`;
     distinct = `区：${distinct}`;
     isp = `运营商：${isp}`;
-    operator = `来访：${operator}`;
+    operator = `服务商：${operator}`;
     lon = `经度：${lon}`;
     lat = `纬度：${lat}`;
-    ip = `IP：${ip}`;
+    ip = `IP地址：${ip}`;
     full_ip = `全IP：${full_ip}`;
     net_str = `网络商：${net_str}`;
     let region = `地区：${country}${country_code}${province}${city}${distinct}${isp}${operator}${lon}${lat}${ip}${full_ip}${net_str}`;
     body={
         title: "网络信息",
-        content: `${country}n${country_code}n${province}n${city}n${distinct}n${isp}n${operator}n${lon}n${lat}n${ip}n${full_ip}n${net_str}\n${region}`,
+        content: `${country}\n${country_code}\n${province}\n${city}\n${distinct}\n${isp}\n${operator}\n${lon}\n${lat}\n${ip}\n${full_ip}\n${net_str}\n${region}`,
         backgroundColor: "#ad8b3d",
         icon: "antenna.radiowaves.left.and.right.circle.fill",
     }
